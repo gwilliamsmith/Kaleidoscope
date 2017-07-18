2d88c03c8394728836854dc1dc20a2b32b9c8ef4
redpox <g.williamsmithiv@gmail.com>
2017-07-18 19:33:32 -0400

    Documentation and small tweaks for default package

--- src/graphvisualizer/GraphNodeColors.java
+++ src/graphvisualizer/GraphNodeColors.java
@@ -1,26 +1,41 @@
-
 package graphvisualizer;
 
 import java.awt.Color;
 
+/**
+ * An enum, defining possible colors for a {@link GraphNode}.
+ */
 public enum GraphNodeColors {
-    DEFAULT_COLOR (Color.BLUE),
-    DEFAULT_EDGE_COLOR (Color.BLACK),
-    DEFAULT_MIDDLE_COLOR (Color.DARK_GRAY),
-    SELECTED_COLOR (Color.GREEN),
-    SELECTED_ADJACENT_COLOR (Color.CYAN),
-    LINE_EVENT_NODE1_COLOR (Color.YELLOW),
-    LINE_EVENT_NODE1_ADJACENT_COLOR (new Color (200,200,0)),
-    LINE_EVENT_NODE2_COLOR (Color.ORANGE),
-    WHITE (Color.WHITE);
-    
+
+    DEFAULT_COLOR(Color.BLUE), //The default color for most nodes
+    DEFAULT_EDGE_COLOR(Color.BLACK), //The default color for a node on the edge of the Graph rectangle
+    DEFAULT_MIDDLE_COLOR(Color.DARK_GRAY), //The default color for a node in the middle row or column of the Graph
+    SELECTED_COLOR(Color.GREEN), //Used when the user has clicked on a node to create a connection
+    SELECTED_ADJACENT_COLOR(Color.CYAN), //Used to highlight nodes eligable for GraphTuple connection by the user
+    LINE_EVENT_NODE1_COLOR(Color.YELLOW), //The first node being selected when setting up a PlaceLineEvent
+    LINE_EVENT_NODE1_ADJACENT_COLOR(new Color(200, 200, 0)), //Highlights nodes adjacent to Line_Event_Node1
+    LINE_EVENT_NODE2_COLOR(Color.ORANGE), //The second node being selected when setting up a PlaceLineEvent
+    WHITE(Color.WHITE);                                                         //White...yeah this one's obvious
+
     private Color color;
 
+    /**
+     * Private constructor, so that the enum can have {@link java.awt.Color}s
+     * associated with them.
+     *
+     * @param in The color to associate with an enum value
+     */
     private GraphNodeColors(Color in) {
         color = in;
     }//end GraphNodeColors constructor
-    
-    public Color getColor(){
+
+    /**
+     * Retrieves the color associated with the enum value
+     *
+     * @return The {@link java.awt.Color} object describing the color for the
+     * enum value
+     */
+    public Color getColor() {
         return color;
     }//end getColor
 }//end GraphNodeColors enum
